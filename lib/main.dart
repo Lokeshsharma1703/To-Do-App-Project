98b353a6571b90c5c791303bfef3ea261a0cb3f0
Lokeshsharma1703 <lokeshsharma17032003@gmail.com>
2022-07-12 13:40:25 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:to_do/Home_screen.dart';
+import 'package:to_do/login.dart';
+
+void main() {
+  runApp(MaterialApp(debugShowCheckedModeBanner: false, home: MyLogin()));
+}
